bd2fa370847be64d46274abf17d82288ab67912d
subin <starbin7788@gmail.com>
2024-10-15 22:05:14 +0900

    feat: 반환타입 ResponseEntity 변경

--- src/main/java/com/semo/semo/domain/user/controller/UserController.java
+++ src/main/java/com/semo/semo/domain/user/controller/UserController.java
@@ -4,6 +4,8 @@ import com.semo.semo.domain.user.model.request.UserSignupReq;
 import com.semo.semo.domain.user.model.response.UserSignupRes;
 import com.semo.semo.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,10 +15,14 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public String signup(@RequestBody UserSignupReq request){ //TODO : 반환 메세지 정하기
-        UserSignupRes signupRes = userService.signup(request);
-        //return ResponseEntity.status(HttpStatus.OK);
-        return "OK";
+    public ResponseEntity<?> signup(@RequestBody UserSignupReq request){
+        try {
+            UserSignupRes signupRes = userService.signup(request);
+            return ResponseEntity.status(HttpStatus.CREATED).body(signupRes);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Signup failed : " +e.getMessage());
+        }
+
     }
 
 }
