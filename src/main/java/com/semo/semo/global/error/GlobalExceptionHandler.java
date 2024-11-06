e7117131fbc306f10e6f413f328fddd0af9bf572
seochae523 <seochae523@naver.com>
2024-11-06 19:56:47 +0900

    Feat : add error handler

--- src/main/java/com/semo/semo/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/semo/semo/global/error/GlobalExceptionHandler.java
@@ -1,15 +1,22 @@
 package com.semo.semo.global.error;
 
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiErrorResponse> handleCustomException(final CustomException ex) {
+        log.error("Error : {}", ex.getErrorCode().getMessage());
+        return ResponseEntity
+                .status(ex.getErrorCode().getStatus())
+                .body(new ApiErrorResponse(ex.getErrorCode().getMessage()));
     }
+
 
 }
