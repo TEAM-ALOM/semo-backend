b7767b0e2c43fa616e503b3fd2ec7674db44480a
subin <starbin7788@gmail.com>
2024-11-13 10:58:33 +0900

    Feat: User pw 속성 삭제

--- src/main/java/com/semo/semo/domain/user/service/UserService.java
+++ src/main/java/com/semo/semo/domain/user/service/UserService.java
@@ -8,7 +8,6 @@ import com.semo.semo.global.error.CustomException;
 import com.semo.semo.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class UserService {
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+
     public UserSignupRes signup(UserSignupReq request){
         // 중복 체크
         userRepository.findByUserId(request.getId())
@@ -27,7 +26,6 @@ public class UserService {
                 });
         User user = User.builder()
                 .userId(request.getId())
-                .pw(passwordEncoder.encode(request.getPw()))
                 .name(request.getName())
                 .nickname(request.getNickname())
                 .role(request.getRole())
