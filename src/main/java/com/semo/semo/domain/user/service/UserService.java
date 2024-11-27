d27b584bfa22ac238146296afcabe9427727cf3f
subin <starbin7788@gmail.com>
2024-11-27 18:53:06 +0900

    Feat: 로그인 자격증명 구현

--- src/main/java/com/semo/semo/domain/user/service/UserService.java
+++ src/main/java/com/semo/semo/domain/user/service/UserService.java
@@ -15,10 +15,13 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.*;
 
@@ -67,7 +70,55 @@ public class UserService {
                         .userId(request.getId())
                         .name(request.getName())
                         .nickname(request.getNickname())
-                        .role(request.getRole())
+                        .role("USER")
+                        .major(major)
+                        .build();
+
+                User savedUser = userRepository.save(user);
+                return new UserSignupRes().toDto(savedUser);
+            }
+        }
+        throw new CustomException(ErrorCode.AUTH_FAIL);
+    }
+
+    public UserSignupRes signupAdmin(UserSignupReq request) {
+        // 중복 체크
+        userRepository.findByUserId(request.getId())
+                .ifPresent(x -> {
+                    throw new CustomException(ErrorCode.USER_ALREADY_EXISTS);
+                });
+
+        // Sejong Auth API 호출 설정
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://auth.imsejong.com/auth?method=DosejongSession";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+
+        // 요청 바디 구성
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("id", request.getId());
+        requestBody.put("pw", request.getPw());
+
+        HttpEntity<Map<String, String>> httpEntity = new HttpEntity<>(requestBody, headers);
+
+        // API 호출 및 응답 처리
+        ResponseEntity<Map> response = restTemplate.exchange(url, HttpMethod.POST, httpEntity, Map.class);
+
+        Map<String, Object> responseBody = response.getBody();
+        if (responseBody != null && "success".equals(responseBody.get("msg"))) {
+            Map<String, Object> result = (Map<String, Object>) responseBody.get("result");
+            if (Boolean.TRUE.equals(result.get("is_auth"))) {
+
+                Map<String, Object> body = (Map<String, Object>) result.get("body");
+                String major = (String) body.get("major");
+
+                // 성공 시 회원 가입 진행
+                User user = User.builder()
+                        .userId(request.getId())
+                        .name(request.getName())
+                        .nickname(request.getNickname())
+                        .role("ADMIN")
                         .major(major)
                         .build();
 
@@ -105,28 +156,29 @@ public class UserService {
             Map<String, Object> result = (Map<String, Object>) responseBody.get("result");
             if (Boolean.TRUE.equals(result.get("is_auth"))) {
 
-//                 //성공 시 로그인 진행
-//                UsernamePasswordAuthenticationToken authenticationToken = new CustomUsernamePasswordAuthenticationToken(request.getId());
-//                authenticationToken.setAuthenticated(true);
-//                 //자격 증명 확인
-//                Authentication authenticate = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+                // 성공 시 SecurityContext에 인증 정보 설정
+                Authentication auth = new UsernamePasswordAuthenticationToken(
+                        user.getUserId(),
+                        null,
+                        user.getAuthorities()
+                );
 
-//                List<GrantedAuthority> authorities = new ArrayList<>();
-//                authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-//
-//                authenticateWithoutPassword(request.getId(), authorities);
+                // 인증 정보를 SecurityContext에 저장하여, 이후 요청에서 인증된 사용자 정보로 처리
+                SecurityContextHolder.getContext().setAuthentication(auth);
 
-                String authToken = JwtUtils.generateRefreshToken(request.getId());
+                // JWT 토큰 생성
+                String accessToken = JwtUtils.generateAccessToken(user.getUserId(),user.getRole());
+                String refreshToken = JwtUtils.generateRefreshToken(user.getUserId());
 
-                // 인증된 사용자를 가져와 refreshToken을 저장
-//                User user = (User) authenticate.getPrincipal();
-
-                user.setRefreshToken(authToken);
+                // Refresh 토큰 저장
+                user.setRefreshToken(refreshToken);
                 userRepository.save(user);
 
                 return UserLoginRes.builder()
                         .id(user.getUserId())
                         .nickname(user.getNickname())
+                        .accessToken(accessToken)
+                        .refreshToken(user.getRefreshToken())
                         .build();
             }
         }
